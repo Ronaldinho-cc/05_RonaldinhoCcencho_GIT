e9a41326ec67361c39da5769db1833bf98722c4f
RonaldinhoCc <ronaldinho.ccencho@vallegrande.edu.pe>
2023-10-05 14:40:40 -0500

    archivos

---
+++ 05_RonaldinhoCcencho/src/S06_05/S06_05.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package S06_05;
+
+/**
+ *
+ * @author GIGABYTE
+ */
+public class S06_05 {
+    
+}
